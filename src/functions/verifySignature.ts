46f39f1301b7b1c241b2607751755808946c4b07
seb0zz <sebastian@elbstack.com>
2020-02-28 17:27:23 +0100

    Fix bitcore message construction

--- src/functions/verifySignature.ts
+++ src/functions/verifySignature.ts
@@ -1,7 +1,7 @@
-import * as BitcoreMessage from 'bitcore-message';
+import * as bitcoreMessage from 'bitcore-message';
 
 export default (message: string, address: string, signature: string): boolean => {
-  const messageToVerify = new BitcoreMessage(message);
+  const messageToVerify = bitcoreMessage(message);
 
   return messageToVerify.verify(address, signature);
 }
