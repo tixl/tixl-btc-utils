0336cafd08a832a868b7446406a8cef2a4f644e0
seb0zz <sebastian@elbstack.com>
2020-03-11 23:38:06 +0700

    Fix pre-transaction fee calculation

--- src/functions/createTransaction.ts
+++ src/functions/createTransaction.ts
@@ -3,20 +3,18 @@ import { Transaction, TransactionInputOrOutput } from '../types';
 import { BLOCKCYPHER_BASE_URL } from '../config';
 import { getTransactionSender } from './shared';
 
-const reduceFeesFromReceiverOutput = (transaction: Transaction, fees: number, outputAddress: string): Transaction => {
-  const clonedTransaction = {
-    ...transaction,
-  };
+const reduceFeesFromReceiverOutput = (outputs: TransactionInputOrOutput[], fees: number, outputAddress: string): TransactionInputOrOutput[] => {
+  const clonedOutputs = [...outputs];
 
-  const outputIndex = clonedTransaction.transactionData.outputs.findIndex((output: TransactionInputOrOutput) => {
+  const outputIndex = clonedOutputs.findIndex((output: TransactionInputOrOutput) => {
     return output.addresses.includes(outputAddress);
   });
 
   if (outputIndex >= 0) {
-    (clonedTransaction.transactionData.outputs[outputIndex] as TransactionInputOrOutput).value = (clonedTransaction.transactionData.outputs[outputIndex] as TransactionInputOrOutput).value - fees;
+    clonedOutputs[outputIndex].value = clonedOutputs[outputIndex].value - fees;
   }
 
-  return clonedTransaction;
+  return clonedOutputs;
 };
 
 export const assertAllOutputsArePositive = (transaction: Transaction) => {
@@ -27,23 +25,40 @@ export const assertAllOutputsArePositive = (transaction: Transaction) => {
   });
 };
 
+/**
+ * @todo - We could extend this function so that it accepts another boolean param `includingFees` which specifies
+ * if the fee should be included in the value or on top
+ *
+ * @param fromAddress The address from which the BTC should be sent
+ * @param toAddress The address which should receive the BTC funds given by `value`
+ * @param value The BTC funds to be sent (*IMPORTANT*: including fees)
+ */
 export default async (fromAddress: string, toAddress: string, value: number): Promise<Transaction> => {
   const inputs = [{ addresses: [fromAddress] }];
   const transactionData = {
+    fees: undefined,
     inputs,
     outputs: [{ addresses: [toAddress], value }],
     preference: 'medium',
   };
-
   const sender = getTransactionSender(inputs as TransactionInputOrOutput[]);
-  try {
-    const { data: { tosign, tx } } = await axios.post(`${BLOCKCYPHER_BASE_URL}/txs/new`, JSON.stringify(transactionData));
 
-    const transaction = reduceFeesFromReceiverOutput({
+  console.log('Doing first request');
+  // we create the transaction first to determine the recommended fee
+  const { data: { tx: { fees } } } = await axios.post(`${BLOCKCYPHER_BASE_URL}/txs/new`, JSON.stringify(transactionData));
+
+  // we remove the fee from the transaction output
+  transactionData.outputs = reduceFeesFromReceiverOutput(transactionData.outputs, fees, toAddress);
+  transactionData.fees = fees;
+
+  try {
+    // create the transaction again with the adjusted fees
+    const { data: { tosign, tx } } = await axios.post(`${BLOCKCYPHER_BASE_URL}/txs/new`, JSON.stringify(transactionData));
+    const transaction: Transaction = {
       sender,
-      transactionData: tx,
       toSign: tosign,
-    }, tx.fees, toAddress);
+      transactionData: tx,
+    };
 
     assertAllOutputsArePositive(transaction);
     return transaction;
