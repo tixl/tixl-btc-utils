960d7b702eaf56c710365f2162c894a196b55dbf
seb0zz <sebastian@elbstack.com>
2020-04-07 15:43:28 +0200

    Add some debugging code

--- src/functions/sendTransactionToNetwork.ts
+++ src/functions/sendTransactionToNetwork.ts
@@ -5,12 +5,14 @@ import { getTransactionSender } from './shared';
 
 export default async (transaction: SignedTransaction): Promise<TransactionInfos> => {
   const { publicKeys, signatures, toSign, transactionData } = transaction;
-  const { data: { confirmations, hash, inputs } } = await axios.post(`${BLOCKCYPHER_BASE_URL}/txs/send`, JSON.stringify({
+  const result = await axios.post(`${BLOCKCYPHER_BASE_URL}/txs/send`, JSON.stringify({
     pubkeys: publicKeys,
     signatures,
     tosign: toSign,
     tx: transactionData,
   }));
+  console.info(JSON.stringify(result.data));
+  const { data: { confirmations, hash, inputs } } = result;
   const sender = getTransactionSender(inputs as TransactionInputOrOutput[]);
   return { confirmations, hash, sender };
 };
